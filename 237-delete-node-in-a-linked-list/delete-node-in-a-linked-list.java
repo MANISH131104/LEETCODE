69fdd416b729c61f6c26eb777f783f7695010f1b
Manish Kumar <manishkumar117694@gmail.com>
2026-08-23 21:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (98.87%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,7 +8,7 @@
  */
 class Solution {
     public void deleteNode(ListNode tar) {
-       tar.val = tar.next.val;
-       tar.next = tar.next.next;
+        tar.val = tar.next.val;
+        tar.next = tar.next.next;
     }
 }
