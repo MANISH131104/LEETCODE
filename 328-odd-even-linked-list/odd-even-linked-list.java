fcd981d2675f1e08ae8ef860f3895cb65bb92496
Manish Kumar <manishkumar117694@gmail.com>
2026-08-28 23:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (89.69%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode temp = head;
+        ListNode dummy1 = new ListNode(-1);
+        ListNode temp1 = dummy1;
+        ListNode dummy2 = new ListNode(-1);
+        ListNode temp2 = dummy2;
+        while(temp != null){
+            temp1.next = temp;
+            temp = temp.next;
+            temp1 = temp1.next;
+            temp2.next = temp;
+            if(temp != null) temp = temp.next;
+            temp2 = temp2.next;
+        }
+        temp1.next = dummy2.next;
+        return dummy1.next;
+    }
+}
